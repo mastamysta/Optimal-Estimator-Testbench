2ed601a698316570599af22464ce4c7bb438711c
Ben Read <benjamin-read@hotmail.co.uk>
2024-05-21 23:06:32 +0100

    Use templates to create basis for matrix lib.

--- estimator_testbench/matrix.h
+++ estimator_testbench/matrix.h
@@ -1,1 +1,29 @@
 #pragma once
+
+// Implementing my own matrices. Partly as a refresher and partly because I really don't need a lot of
+// functionality so BLAS is probably overkill.
+
+// Just 2D matrices for now...
+
+
+template <int... Is> // Base
+struct matrix_wrapper
+{
+	using type = float;
+};
+
+template <int I, int... Is> // Specialise
+struct matrix_wrapper<I, Is...>
+{
+	using type = matrix_wrapper<Is...>::type[I];
+};
+
+template <int... Is>
+using matrix_blob = matrix_wrapper<Is...>::type;
+
+template <int... Is>
+class matrix
+{
+private:
+	matrix_blob<Is...> v;
+};
--- estimator_testbench/testbench.cpp
+++ estimator_testbench/testbench.cpp
@@ -5,4 +5,6 @@
 int main()
 {
 	std::cout << "Hello world!\n";
+
+	matrix<2,2> my_test_matrix;
 }
