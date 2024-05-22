d861280fe2a820ef8509441ea45de2cb6c8fd0cd
Ben Read <benjamin-read@hotmail.co.uk>
2024-05-22 19:30:27 +0100

    Reimplement matrices with support for 2D matrices only. Keep it simple, stupid!

--- estimator_testbench/matrix.h
+++ estimator_testbench/matrix.h
@@ -6,24 +6,70 @@
 // Just 2D matrices for now...
 
 
-template <int... Is> // Base
-struct matrix_wrapper
-{
-	using type = float;
-};
 
-template <int I, int... Is> // Specialise
-struct matrix_wrapper<I, Is...>
-{
-	using type = matrix_wrapper<Is...>::type[I];
-};
-
-template <int... Is>
-using matrix_blob = matrix_wrapper<Is...>::type;
-
-template <int... Is>
+template <size_t I, size_t J>
 class matrix
 {
-private:
-	matrix_blob<Is...> v;
+public:
+
+	float vals[I][J];
+	size_t X = I;
+	size_t Y = J;
+
+	void setvals(std::vector<float> init_vals)
+	{
+		float* f = (float*)this->vals;
+
+		for (float val : init_vals)
+		{
+			*f = val;
+			f++;
+		}
+	}
+
+	void displayvals()
+	{
+		for (int i = 0; i < I; i++)
+		{
+			for (int j = 0; j < J; j++)
+				std::cout << this->vals[i][j] << " ";
+			std::cout << "\n";
+		}
+	}
+
+	matrix<J, I> transpose()
+	{
+		std::vector<float> tmp;
+
+		for (int j = 0; j < J; j++)
+			for (int i = 0; i < I; i++)
+				tmp.push_back(this->vals[i][j]);
+
+		matrix<J, I> ret;
+		ret.setvals(tmp);
+		return ret;
+	}
+
 };
+
+template <size_t I, size_t J, size_t K>
+matrix <I, K> operator*(matrix<I, J> lhs, matrix<J, K> rhs)
+{
+	//				 <- Y ->
+	// [X x Y] -> /\[		]
+	//			  X [		]
+	//			  \/[		]
+
+	// for Each column of rhs
+	//		for each row in rhs
+	//			for each
+
+	matrix<I, K> out;
+	
+	for (int i = 0; i < I; i++)
+		for (int k = 0; k < K; k++)
+			for (int j = 0; j < J; j++)
+				out.vals[i][j] += lhs.vals[i][j] * rhs.vals[j][k];
+
+	return out;
+}
--- estimator_testbench/testbench.cpp
+++ estimator_testbench/testbench.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <vector>
+
 
 #include "matrix.h"
 
+#ifdef RUN_TESTS
+
+void matrix_tests()
+{
+	matrix<3, 3> A;
+	matrix<3, 1> B;
+
+	A.setvals({ 1, 0, 2, 1, -1, 2, 0, 3, 3 });
+	B.setvals({ 1, 0, 0 });
+
+	A.displayvals();
+
+	std::cout << "\n\n";
+
+	B.displayvals();
+
+	std::cout << "\n\n";
+
+	(A * B).displayvals();
+
+	std::cout << "\n\n";
+
+	A.transpose().displayvals();
+}
+
+#endif
+
 int main()
 {
-	std::cout << "Hello world!\n";
 
-	matrix<2,2> my_test_matrix;
+#ifdef RUN_TESTS
+	matrix_tests();
+#endif
+
+	return 0;
 }
